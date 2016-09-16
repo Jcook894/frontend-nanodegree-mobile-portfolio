fa00899390489b8fe5b49a5a2d3edb1d0b0fc8bc
Jcook894 <Jcook894@icloud.com>
2016-09-16 02:11:12 -0400

    minified the pizza html and main js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
+var webserver = require('gulp-webserver');
+var htmlmin = require('gulp-htmlmin');
 
 // Minify's and compresses the main.js file
 gulp.task('compress', function (cb) {
@@ -14,4 +15,20 @@ gulp.task('compress', function (cb) {
   );
 });
 
-gulp.task('default',['compress']);
+//Minify's pizza html.
+gulp.task('minify', function() {
+  return gulp.src('./views/pizza.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+
+//still working on webserver task
+gulp.task('serve', function() {
+  gulp.src('dist')
+    .pipe(webserver({
+      port:'9090',
+      livereload: true,
+      open: true
+    }));
+});
